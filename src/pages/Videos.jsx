141b1e9fab9a43b4c90a5bf65c50ea28059afdc1
Jaehee Han <baskinhee@rtruesoft.kr>
2022-12-02 17:44:54 +0900

    교회 유튜브 불러오기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,23 +1,28 @@
 import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const API = "AIzaSyB8Qzow1XzdvBiLTO6D86SwR1_j3JiYeQU"
 const channelId ="UCpNfYpL9wvjEwWDJikG1KUQ"
 
-let fetchUrl = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResult=10`
-console.log(fetchUrl)
+// let fetchUrl = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResult=10`
+// console.log(fetchUrl)
 export const Videos = () => {
-  const [allVideos, setAllVideos] = useState([])
-  useEffect(() => {
-    fetch(fetchUrl).then((response) => response.json()).then((resJson) => {
-      const result = resJson.items.map(doc => ({
-        ...doc,
-        VideoLink: "https://www.youtube.com/embed/"+doc.id.videoId
-      }))
-      setAllVideos(result)
-    })
-  }, [])
+  const [playlist, setPlaylist] = useState([]);
 
-  console.log(allVideos)
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&key=${API}`
+      )
+      .then((res) => {
+        console.log(res);
+        setPlaylist(res.data.items);
+      })
+      .catch(() => {});
+  }, []);
+
+  console.log(playlist);
+
   return (
     <div>
       <h1>VideoPages</h1>
