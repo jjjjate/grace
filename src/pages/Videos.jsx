03246f90ce941ba830fba48dcb6bf49034a5d705
Jaehee Han <baskinhee@rtruesoft.kr>
2022-12-02 17:30:03 +0900

    found channelId

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 
 const API = "AIzaSyB8Qzow1XzdvBiLTO6D86SwR1_j3JiYeQU"
-const channelId ="@user-ib3bt2yo1h"
+const channelId ="UCpNfYpL9wvjEwWDJikG1KUQ"
 
 let fetchUrl = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResult=10`
 console.log(fetchUrl)
