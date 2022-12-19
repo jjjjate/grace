c5bc5025a67e5c6030af55c68091364d8528c479
Jaehee Han <baskinhee@gmail.com>
2022-12-19 23:51:15 +0900

    youtube videos

--- src/components/videos/Icons.jsx
+++ src/components/videos/Icons.jsx
@@ -4,34 +4,41 @@ import { Link } from 'react-router-dom';
 const Icons = () => {
   return (
     <div>
-      <div className="flex flex-none px-20">
+      <div className="flex flex-none px-16">
         <Link to="/home">
           <img className="w-10" src="/img/home.svg" alt="homeLogo"/>
         </Link>
         <img className="w-10 ml-4" src="/img/arrow.svg" alt="arrowLogo"/>
         <p className="pt-2 ml-4 font-extrabold text-sm">설교와 찬양</p>
       </div>
-      <div className="flex flex-none px-20 pt-16">
+      <div className="flex flex-none px-16 py-8">
         <h1 className="font-extrabold text-4xl">설교말씀</h1>
       </div>
-      <div className="flex flex-none px-20 py-8">
-        <button 
-        className="rounded-2xl text-white bg-orange-400 border-2 border-orange-400 w-24 text-sm"
+      <div className="flex py-5 sm:px-16">
+        <div 
+        className="text-center rounded-2xl text-white bg-orange-400 border-2 border-orange-400 w-22 text-sm
+        hover:cursor-pointer"
         >
           주일오전예배
-        </button>
-        <button className="ml-4 rounded-2xl bg-white border-2 border-gray-400 w-24 text-sm
-        hover:bg-orange-400 hover:text-white hover:border-0">
+        </div>
+        <div 
+        className="text-center ml-2 rounded-2xl bg-white border-2 border-gray-400 w-22 text-sm
+        hover:bg-orange-400 hover:text-white hover:border-orange-400 hover:cursor-pointer"
+        >
           주일오후예배
-        </button>
-        <button className="ml-4 rounded-2xl bg-white border-2 border-gray-400 w-24 text-sm
-        hover:bg-orange-400 hover:text-white hover:border-0">
+        </div>
+        <div 
+        className="text-center ml-2 rounded-2xl bg-white border-2 border-gray-400 w-24 text-sm
+        hover:bg-orange-400 hover:text-white hover:border-orange-400 hover:cursor-pointer"
+        >
           수요예배
-        </button>
-        <button className="ml-4 rounded-2xl bg-white border-2 border-gray-400 w-24 text-sm
-        hover:bg-orange-400 hover:text-white hover:border-0">
+        </div>
+        <div 
+        className="text-center ml-2 rounded-2xl bg-white border-2 border-gray-400 w-24 text-sm
+        hover:bg-orange-400 hover:text-white hover:border-orange-400 hover:cursor-pointer"
+        >
           금요기도회
-        </button>
+        </div>
       </div>
     </div>
   )
--- src/pages/MorningVideos.jsx
+++ src/pages/MorningVideos.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
 import Icons from '../components/videos/Icons';
 
 const API = "AIzaSyB8Qzow1XzdvBiLTO6D86SwR1_j3JiYeQU"
@@ -13,7 +12,7 @@ export const MorningVideos = () => {
   useEffect(() => {
     axios
       .get(
-        `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playListId}&maxResults=13&key=${API}`
+        `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playListId}&maxResults=7&key=${API}`
       )
       .then((res) => {
         setMorningVideo(res.data.items);
@@ -60,72 +59,94 @@ export const MorningVideos = () => {
       <div>
         {morningVideo.slice(0,1).map((title, index) => {
           return (
-            <div key={index}>
-              <h1>주일 오전 예배</h1>
-              <h1>{title.snippet.title}</h1>
-              <span>{title.snippet.publishedAt.replace('T', ' ').substring(0, 16)}</span>
+            <div className="py-4" key={index}>
+              <h1 className="pb-2 text-extrabold text-2xl text-orange-400 px-16">주일 오전 예배</h1>
+              <h1 className="text-2xl text-extrabold px-16">{title.snippet.title}</h1>
+              <div className="py-2 px-16">
+                <p className="text-extralight text-sm text-gray-500">
+                  {title.snippet.publishedAt.replace('T', ' ').substring(0, 11)}
+                </p>
+              </div>
             </div>
           )
         })}
       </div>
-      <div className="lg:block hidden">
-        <div className="grid grid-cols-3 gap-4">
-          {morningVideo.slice(1).reverse().map((items, index) => {
-            return (
-              <div key={index}>
-                <h2>{items.snippet.title}</h2>
-                <span className="font-extralight">{items.snippet.publishedAt.replace('T', ' ').substring(0, 16)}</span>
-                <iframe
-                  title="subFrames"
-                  src={videoLink + items.snippet.resourceId.videoId}
-                  width="100%"
-                  allowFullScreen
-                />
-                <p>{items.snippet.description}</p>
-              </div>
-            )
-          })}
+      <div className="border-b-2 pb-16"></div>
+      <div className="py-4 px-16">
+        <h1 className="text-2xl text-extrabold">말씀 요약</h1>
+      </div>
+      <div className="container mx-auto px-16 py-16">
+        <div className="grid pb-4">
+          <h1 className="font-extrabold text-4xl">다른 설교말씀</h1>
+        </div>
+        <div className="lg:block hidden">
+          <div className="grid grid-cols-3 gap-4">
+            {morningVideo.slice(1).reverse().map((items, index) => {
+              return (
+                <div key={index}>
+                  <iframe
+                    title="subFrames"
+                    src={videoLink + items.snippet.resourceId.videoId}
+                    width="100%"
+                    allowFullScreen
+                  />
+                  <h2>{items.snippet.title}</h2>
+                  <span className="font-extralight">{items.snippet.publishedAt.replace('T', ' ').substring(0, 11)}</span>
+                  <p>{items.snippet.description}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className="sm:hidden md:block lg:hidden hidden">
+          <div className="grid grid-cols-2 gap-4">
+            {morningVideo.slice(1).reverse().map((items, index) => {
+              return (
+                <div key={index}>
+                  <iframe
+                    title="subFrames"
+                    src={videoLink + items.snippet.resourceId.videoId}
+                    width="100%"
+                    height="350px"
+                    allowFullScreen
+                  />
+                  <h2>{items.snippet.title}</h2>
+                  <span className="font-extralight">{items.snippet.publishedAt.replace('T', ' ').substring(0, 11)}</span>
+                  <p>{items.snippet.description}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className="md:hidden lg:hidden">
+          <div className="grid grid-cols-1">
+            {morningVideo.slice(1).reverse().map((items, index) => {
+              return (
+                <div key={index}>
+                  <iframe
+                    title="subFrames"
+                    src={videoLink + items.snippet.resourceId.videoId}
+                    width="100%"
+                    height="350px"
+                    allowFullScreen
+                  />
+                  <h2>{items.snippet.title}</h2>
+                  <span className="font-extralight">{items.snippet.publishedAt.replace('T', ' ').substring(0, 11)}</span>
+                  <p>{items.snippet.description}</p>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-      <div className="sm:hidden md:block lg:hidden hidden">
-        <div className="grid grid-cols-2 gap-4">
-          {morningVideo.slice(1).reverse().map((items, index) => {
-            return (
-              <div key={index}>
-                <h2>{items.snippet.title}</h2>
-                <span className="font-extralight">{items.snippet.publishedAt.replace('T', ' ').substring(0, 16)}</span>
-                <iframe
-                  title="subFrames"
-                  src={videoLink + items.snippet.resourceId.videoId}
-                  width="100%"
-                  height="350px"
-                  allowFullScreen
-                />
-                <p>{items.snippet.description}</p>
-              </div>
-            )
-          })}
-        </div>
-      </div>
-      <div className="md:hidden lg:hidden">
-        <div className="grid grid-cols-1">
-          {morningVideo.slice(1).reverse().map((items, index) => {
-            return (
-              <div key={index}>
-                <h2>{items.snippet.title}</h2>
-                <span className="font-extralight">{items.snippet.publishedAt.replace('T', ' ').substring(0, 16)}</span>
-                <iframe
-                  title="subFrames"
-                  src={videoLink + items.snippet.resourceId.videoId}
-                  width="100%"
-                  height="350px"
-                  allowFullScreen
-                />
-                <p>{items.snippet.description}</p>
-              </div>
-            )
-          })}
-        </div>
+      <div>
+        더 많은 영상은 
+        <a 
+        className="rounded-md bg-red-500 text-black hover:text-red-600 hover:bg-black" 
+        href="https://m.youtube.com/channel/UCpNfYpL9wvjEwWDJikG1KUQ"
+        >
+          유튜브
+        </a>
       </div>
     </div>
   )
